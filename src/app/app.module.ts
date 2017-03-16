8caee34d60c3014e1221bdaa138b77a4ae614fd8
rayji951011 <rayji951011@gmail.com>
2017-03-16 14:18:43 +0000

    Made some changes to the style, added in Contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponents, AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     AppComponents,
-    AboutComponent
+    AboutComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { ProductComponent } from './product/product.component';
 import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
  
 export const AppRoutes: any = [
     { path: "product", component: ProductComponent },
     { path: "about", component: AboutComponent },
+    { path: "contact-us", component: ContactUsComponent },
 ];
  
 export const AppComponents: any = [
